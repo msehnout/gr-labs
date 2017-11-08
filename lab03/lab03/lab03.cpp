c0deb53cb9415d815783b92efc14604600ff3b29
Sehny <msehnout@users.noreply.github.com>
2017-11-08 15:58:22 +0200

    change encoding

--- lab03/lab03/lab03.cpp
+++ lab03/lab03/lab03.cpp
@@ -109,7 +109,7 @@ void mainLoop()
 
         // Task 2: perspective projection
         
-        // Projection matrix: 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+        // Projection matrix: 45ï¿½ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
         projectionMatrix = perspective(radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
         // Task 2.1: change z translation and define MVP
         modelMatrix = translate(mat4(), vec3(0.0, 0.0, -10));
